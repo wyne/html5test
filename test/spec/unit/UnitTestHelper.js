0068591c652d9b5ab2b4b5233afb52dcf6c585ff
Justin Wyne <justin@stackmob.com>
2013-04-04 11:36:00 -0700

    moar test fix

--- test/spec/unit/UnitTestHelper.js
+++ test/spec/unit/UnitTestHelper.js
@@ -2,7 +2,6 @@
  * Unit Test that `obj.sdkMethod` will hit `endPoint` with `verb` request.
  */
 function expectEndpoint(obj, methodParams, sdkMethod, verb, endPoint) {
-  
   var methodParams = methodParams || [];
   methodParams.push({
     done: function(model, params, method) {
@@ -20,14 +19,32 @@ beforeEach(function() {
     toCall: function(endPoint, verb) {
       return this.actual['url'].indexOf(endPoint) > -1 && this.actual['type'] === verb;
     },
-    
+
     toHaveParam: function(key, value) {
       return _.contains(this.actual['data'].split('&'), (key + '=' + value));
     },
-    
+
     toHaveJSONWith: function(key, value) {
       return this.actual['data']['key'] === value;
     }
   });
-  
+
 });
+
+function generateURL(relativePath) {
+  if (StackMob['useRelativePathForAjax'] === true) {
+    return window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + '/' + relativePath;
+  } else {
+    switch (StackMob.secure){
+      case StackMob.SECURE_ALWAYS:
+        return "https://" + StackMob.getBaseURL() + relativePath;
+        break;
+      case StackMob.SECURE_NEVER:
+        return "http://" + StackMob.getBaseURL() + relativePath;
+        break;
+      case StackMob.SECURE_MIXED:
+        return window.location.protocol + "//" + StackMob.getBaseURL() + relativePath;
+        break;
+    }
+  }
+}
